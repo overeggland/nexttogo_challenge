8b748d93bf8f0a136343e961f8d0e67e2f78cde9
Xavier Zhang <overeggland@gmail.com>
2023-07-02 21:00:03 +1000

    Feat: add filter text

--- Neds/View/Picker.swift
+++ Neds/View/Picker.swift
@@ -10,19 +10,22 @@ import SwiftUI
 struct Picker: View {
     @ObservedObject var vm : PickerViewModel
     var body: some View {
-        HStack(
-            alignment: .bottom,
-            spacing: Space_Line
-        ) {
-            ForEach(vm.categories.indices, id: \.self) { index in
-                let category: Category = vm.categories[index]
-                CategoryCell(model: category).onTapGesture {
-                    vm.toggle(seletedIndex: index)
-                 }.cornerRadius(15)
-                  .accessibilityElement()
-                  .accessibilityLabel(category.categoryName)
-            }
-        }.frame(height: 100).padding(.horizontal, 15)
+        VStack {
+            HStack(
+                alignment: .bottom,
+                spacing: Space_Line
+            ) {
+                ForEach(vm.categories.indices, id: \.self) { index in
+                    let category: Category = vm.categories[index]
+                    CategoryCell(model: category).onTapGesture {
+                        vm.toggle(seletedIndex: index)
+                     }.cornerRadius(15)
+                      .accessibilityElement()
+                      .accessibilityLabel(category.categoryName)
+                }
+            }.frame(height: 100).padding(.horizontal, 15)
+            Text("Tap these button to filter races")
+        }
     }
 }
 
